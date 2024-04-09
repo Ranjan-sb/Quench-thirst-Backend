6e0c1337133b58577d4ae2431820241b4e242e72
Ranjan-sb <165002110+Ranjan-sb@users.noreply.github.com>
2024-04-09 13:29:10 +0530

    Update dbConfig.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const configureDB = async ()=>{
     try {
-        const db = await mongoose.connect('mongodb://localhost:27017/qt-2024')
+        const db = await mongoose.connect('mongodb://127.0.0.1:27017/qt-2024')
         console.log('Successfully Connected to db')
     } catch(error) {
         console.log('error connecting to db : ',error)
